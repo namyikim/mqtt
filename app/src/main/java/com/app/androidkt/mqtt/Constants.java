1fb4f0a99091e8dbc152dac68a6ddf51b1a27c97
Namyi ,Kim <30651066+namyikim@users.noreply.github.com>
2019-10-16 08:09:41 +0900

    adding sound volume function

--- app/src/main/java/com/app/androidkt/mqtt/Constants.java
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -8,8 +8,24 @@ public class Constants {
 
     public static final String MQTT_BROKER_URL = "tcp://broker.shiftr.io";
 
+    public static final String PUBLISH_TOPIC = "SoundVolume";
+
+    public static final String CLIENT_ID = "SoundVolume";
+
+    public static final String USERNAME = "f3500d0a";
+
+    public static final String PASSWORD = "f96fbac0106a7698";
+
+    /*
+    public static final String MQTT_BROKER_URL = "tcp://broker.shiftr.io";
+
     public static final String PUBLISH_TOPIC = "Assistant";
 
     public static final String CLIENT_ID = "Assistant";
+
+    public static final String USERNAME = "24ab938f";
+
+    public static final String PASSWORD = "811386c10d01978a";
+    */
 }
 
