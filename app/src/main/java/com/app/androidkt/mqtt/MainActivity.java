51015a1178a37d29de275e2abc93bbe43e56b194
Namyi ,Kim <30651066+namyikim@users.noreply.github.com>
2019-10-16 06:18:15 +0900

    adding voice recognition function

--- app/src/main/java/com/app/androidkt/mqtt/MainActivity.java
+++ app/src/main/java/com/app/androidkt/mqtt/MainActivity.java
@@ -2,15 +2,21 @@ package com.app.androidkt.mqtt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText textMessage, subscribeTopic, unSubscribeTopic;
     private Button publishMessage, subscribe, unSubscribe;
+
+    //namyi47.kim
+    private Button sttBtn;
+    private TextView textView;
+
+    Intent intent_voicerecognizer;
+    SpeechRecognizer mRecognizer;
+    //namyi47.kim
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +49,21 @@ public class MainActivity extends AppCompatActivity {
 
         subscribeTopic = (EditText) findViewById(R.id.subscribeTopic);
         unSubscribeTopic = (EditText) findViewById(R.id.unSubscribeTopic);
+
+        //namyi47.kim
+        textView = (TextView)findViewById(R.id.sttresult);
+        sttBtn = (Button) findViewById(R.id.sttstart);
+
+        intent_voicerecognizer=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent_voicerecognizer.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+        intent_voicerecognizer.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"en-EN");
+        sttBtn.setOnClickListener(v ->{
+            mRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
+            mRecognizer.setRecognitionListener(listener);
+            mRecognizer.startListening(intent_voicerecognizer);
+        });
+        //namyi47.kim
+
         client = pahoMqttClient.getMqttClient(getApplicationContext(), Constants.MQTT_BROKER_URL, Constants.CLIENT_ID);
 
         publishMessage.setOnClickListener(new View.OnClickListener() {
@@ -83,4 +112,126 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, MqttMessageService.class);
         startService(intent);
     }
+
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {}
+
+        @Override
+        public void onRmsChanged(float rmsdB) {}
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {}
+
+        @Override
+        public void onEndOfSpeech() {}
+
+        @Override
+        public void onError(int error) {
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+            ArrayList<String> matches =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            for(int i = 0; i < matches.size() ; i++){
+
+                textView.setText(matches.get(i));
+
+                if(matches.get(i).contains("hi"))
+                {
+                    try {
+                        pahoMqttClient.publishMessage(client, "hi", 1, Constants.PUBLISH_TOPIC);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, "hi", Toast.LENGTH_SHORT).show();
+                }
+                if(matches.get(i).contains("Subway"))
+                {
+                    try {
+                        pahoMqttClient.publishMessage(client, "subway", 1, Constants.PUBLISH_TOPIC);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, "subway", Toast.LENGTH_SHORT).show();
+                }
+                if(matches.get(i).contains("car"))
+                {
+                    try {
+                        pahoMqttClient.publishMessage(client, "car", 1, Constants.PUBLISH_TOPIC);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, "car", Toast.LENGTH_SHORT).show();
+                }
+                if(matches.get(i).contains("ambulance"))
+                {
+                    try {
+                        pahoMqttClient.publishMessage(client, "ambulance", 1, Constants.PUBLISH_TOPIC);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, "ambulance", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {}
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {}
+    };
 }
