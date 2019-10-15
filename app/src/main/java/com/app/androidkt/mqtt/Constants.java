7f35c319aa9a7cf574c6af39d5ab49351530ff59
Namyi ,Kim <30651066+namyikim@users.noreply.github.com>
2019-10-16 03:13:35 +0900

    initial upload

---
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,15 @@
+package com.app.androidkt.mqtt;
+
+/**
+ * Created by brijesh on 20/4/17.
+ */
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://broker.shiftr.io";
+
+    public static final String PUBLISH_TOPIC = "Assistant";
+
+    public static final String CLIENT_ID = "Assistant";
+}
+
