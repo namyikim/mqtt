e9b011e3b45e0c9ef723ec0b24f6ec461e657572
Namyi ,Kim <30651066+namyikim@users.noreply.github.com>
2019-10-22 14:24:23 +0900

    fixing bug

--- app/src/main/java/com/app/androidkt/mqtt/Constants.java
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -5,7 +5,7 @@ package com.app.androidkt.mqtt;
  */
 
 public class Constants {
-
+/*
     public static final String MQTT_BROKER_URL = "tcp://broker.shiftr.io";
 
     public static final String PUBLISH_TOPIC = "SoundVolume";
@@ -15,17 +15,28 @@ public class Constants {
     public static final String USERNAME = "f3500d0a";
 
     public static final String PASSWORD = "f96fbac0106a7698";
+*/
 
-    /*
-    public static final String MQTT_BROKER_URL = "tcp://broker.shiftr.io";
+    public static final String MQTT_BROKER_URL = "wss://broker.shiftr.io";
 
     public static final String PUBLISH_TOPIC = "Assistant";
 
     public static final String CLIENT_ID = "Assistant";
 
+    // nathon server
     public static final String USERNAME = "24ab938f";
-
     public static final String PASSWORD = "811386c10d01978a";
+
+    //namyi server
+    /*
+    public static final String USERNAME = "f3500d0a";
+    public static final String PASSWORD = "f96fbac0106a7698";
     */
+
+
+    public static final boolean RETAINED = false;
+
+
+
 }
 
